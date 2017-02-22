3d8f99fd0e90a5547eedd308901cd91051c80b88
Truong Thanh Son <thanhson.truong@gmail.com>
2017-02-22 17:14:10 +0700

    Initial commit

---
+++ app/src/main/java/com/sontruong/appsound/MainActivity.java
@@ -0,0 +1,83 @@
+package com.sontruong.appsound;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        verifyStoragePermissions(this);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new CustomWaveformFragment())
+                    .commit();
+        }
+    }
+
+    public static class CustomWaveformFragment extends WaveformFragment {
+
+        /**
+         * Provide path to your audio file.
+         *
+         * @return
+         */
+        @Override
+        protected String getFileName() {
+            return Environment.getExternalStorageDirectory() + "/SonTung.mp3";
+        }
+
+        /**
+         * Optional - provide list of segments (start and stop values in seconds) and their corresponding colors
+         *
+         * @return
+         */
+        @Override
+        protected List<Segment> getSegments() {
+            return Arrays.asList(
+                    new Segment(55.2, 55.8, Color.rgb(238, 23, 104)),
+                    new Segment(56.2, 56.6, Color.rgb(238, 23, 104)),
+                    new Segment(58.4, 59.9, Color.rgb(184, 92, 184)));
+        }
+    }
+}
